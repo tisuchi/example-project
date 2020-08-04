84c53a93d2c306ec44e9d8c7bb5ddfe52002b4ee
Thouhedul Islam <thouhedul@cartx.io>
2020-08-04 21:41:12 +0600

    Added TransactionController

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Repositories\TransactionRepository;
+use App\Http\Requests\TransactionPost;
 
 class TransactionController extends Controller
 {
-    //
+    /**
+     * Show the top up form
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('topup.create');
+    }
+
+    /**
+     * Store transaction data and update total amount.
+     *
+     * @param TransactionPost $request
+     * @param $walletId
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(TransactionPost $request, $walletId)
+    {
+        $topUp = (new TransactionRepository())->store($request, $walletId);
+
+        if (! $topUp){
+            return redirect()->back()->with('danger', 'Something went wrong. Try again later.');
+        }
+
+        if ($topUp == 'insufficient'){
+            return redirect()->route('wallets.index')->with('danger', 'Insufficient amount.');
+        }
+
+        return redirect()->route('wallets.index')
+            ->with('success', 'You have done top up successfully.');
+    }
 }
