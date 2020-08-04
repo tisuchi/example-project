8bb67d0e6b433432c154d944e376afedcf79e2bd
Thouhedul Islam <thouhedul@cartx.io>
2020-08-04 21:41:47 +0600

    Added TransactionPost

---
+++ app/Http/Requests/TransactionPost.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'amount' => 'required',
+            'transaction_type_id' => 'required',
+        ];
+    }
+}
