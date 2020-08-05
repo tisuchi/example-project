499ff21fbccb1695a81f404e08d8a6db1dac6c02
tisuchi <tisuchi@gmail.com>
2020-08-05 16:54:58 +0600

    Added isWalletOwner helper

--- app/Utils/Helpers.php
+++ app/Utils/Helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Wallet;
+use Illuminate\Support\Facades\Auth;
+
 if(! function_exists('walletTypes')){
     function walletTypes(){
         return \App\WalletType::get();
@@ -9,5 +12,15 @@ if(! function_exists('walletTypes')){
 if(! function_exists('transationTypes')){
     function transationTypes(){
         return \App\TransactionType::get();
+    }
+}
+
+if(! function_exists('isWalletOwner')){
+    function isWalletOwner($wallet){
+        if(Auth::user()->id != $wallet->user_id){
+            abort(403);
+        }
+
+        return true;
     }
 }
