943b808d60df5952a075eceda88de2b5dc1afcc8
tisuchi <tisuchi@gmail.com>
2020-08-06 11:13:12 +0600

    Added comments.

--- app/Wallet.php
+++ app/Wallet.php
@@ -13,11 +13,22 @@ class Wallet extends Model
         'total',
     ];
 
+    /**
+     * A wallet is belongsTo a walletType
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function walletType()
     {
         return $this->belongsTo(WalletType::class, 'type_id')->withDefault();
     }
 
+    /**
+     * o2m
+     * A wallet has many trnasactions.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function transactions()
     {
         return $this->hasMany(Transaction::class)->latest();
