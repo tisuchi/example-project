cd8584418777a39928a1aecfd3346dacf8858607
tisuchi <tisuchi@gmail.com>
2020-08-06 11:10:21 +0600

    Added comments in reportRepository.

--- app/Http/Repositories/ReportRepository.php
+++ app/Http/Repositories/ReportRepository.php
@@ -6,13 +6,24 @@ use App\Wallet;
 
 class ReportRepository
 {
+    /**
+     * Return all the wallets of a user
+     *
+     * @return mixed
+     */
     public function index()
     {
         return auth()->user()->wallets;
     }
 
+    /**
+     * Return tranasction details of a wallet
+     *
+     * @param $walletId
+     * @return mixed
+     */
     public function show($walletId)
     {
-        return Wallet::where('id', $walletId)->with('transactions.transactionType')->first();
+        return Wallet::where('id', $walletId)->with('transactions.transactionType')->firstOrFail();
     }
 }
