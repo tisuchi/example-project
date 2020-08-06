123fc764d5edc45009ad63e9c4cb840f2f91b3dd
tisuchi <tisuchi@gmail.com>
2020-08-06 11:06:14 +0600

    Added report controller.

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Repositories\ReportRepository;
 use Illuminate\Http\Request;
 
 class ReportsController extends Controller
 {
     public function index()
     {
-        
+        $reports = (new ReportRepository())->index();
+
+        return view('reports.index')
+            ->with([
+                'reports' => $reports
+            ]);
+    }
+
+    public function show($walletId)
+    {
+        $report = (new ReportRepository())->show($walletId);
+
+        return view('reports.show')
+            ->with([
+                'report' => $report
+            ]);
     }
 }
